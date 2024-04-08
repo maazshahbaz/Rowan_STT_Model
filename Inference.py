54d6bfee71d820fdc4a1bcdec476cb3c81b8baf0
maazshahbaz <102242462+maazshahbaz@users.noreply.github.com>
2024-04-08 03:29:12 -0400

    Inference.py

--- Inference.py
+++ Inference.py
@@ -1,16 +1,16 @@
 from datasets import load_dataset, Audio
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
-import torch
-from evaluate import load
 from jiwer import wer
+import torch
+
+device = torch.device("cuda:0")
 
 # Load the processor and model
-processor = WhisperProcessor.from_pretrained("ashfaq93/Model_1")
-model = WhisperForConditionalGeneration.from_pretrained("ashfaq93/Model_1").to("cuda:1")
+processor = WhisperProcessor.from_pretrained("ashfaq93/Adapter_2")
+model = WhisperForConditionalGeneration.from_pretrained("ashfaq93/Adapter_2").to(device)
 
 # Load the dataset
-dataset = load_dataset("load_data_here")
-
+dataset = load_dataset("Load_Data_Here")
 # Remove unnecessary columns
 dataset = dataset.remove_columns(["id", "segment_start_time", "segment_end_time", "duration"])
 
@@ -31,7 +31,7 @@ for i in range(len(dataset["test"])):
     input_features = processor(sample["audio"]["array"], sampling_rate=sample["audio"]["sampling_rate"], return_tensors="pt").input_features 
 
     # Move the input features to the GPU
-    input_features = input_features.to("cuda:1")
+    input_features = input_features.to(device)
 
     # Generate token IDs using the model
     predicted_ids = model.generate(input_features)
